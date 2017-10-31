a08ff5410cd7ad48a71fcefbddd19b6503d237b4
Pedro Piñera <pepibumur@gmail.com>
2017-10-31 11:01:28 +0100

    Create project

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :osx, '10.12'
+
+target 'XcodePM' do
+
+  use_frameworks!
+
+  pod "xcproj", "~> 1.2"
+
+end
---
+++ XcodePM/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XcodePM
+//
+//  Created by Pedro Piñera Buendía on 31.10.17.
+//  Copyright © 2017 microfeatures. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
