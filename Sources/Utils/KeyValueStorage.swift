1e1905e85eff56e2417130731d66f0944c3ed6b5
Pedro Piñera <pepibumur@gmail.com>
2017-11-21 11:01:10 +0100

    Some improvements

--- Sources/Utils/KeyValueStorage.swift
+++ Sources/Utils/KeyValueStorage.swift
@@ -1,13 +1,24 @@
 import Foundation
 import RxSwift
 
-class KeyValueStorage: ReadWriteRepository, ObservableRepository {
+class KeyValueStorage {
+    
+    enum Change<T> {
+        case update(T)
+        case remove
+        var value: T? {
+            switch self {
+            case .update(let value): return value
+            case .remove: return nil
+            }
+        }
+    }
     
     // MARK: - Private
     
     private let name: String
     private let userDefaults: UserDefaults
-    private var subjects: [String: PublishSubject<RepositoryChange<Any>>] = [:]
+    private var subjects: [String: Any] = [:]
     
     // MARK: - Init
     
@@ -16,13 +27,71 @@ class KeyValueStorage: ReadWriteRepository, ObservableRepository {
         self.userDefaults = userDefaults
     }
     
-    // MARK: - Fileprivate
+    // MARK : - Internal
     
-    private func readDictionary() -> [String: Data] {
-        return (userDefaults.value(forKey: name) as? [String: Data]) ?? [:]
+    func observe<T>(key: String) -> Observable<Change<T>> {
+        var subject: PublishSubject<Change<T>>!
+        if let _subject = subjects[key] {
+            subject = _subject as? PublishSubject<Change<T>>
+        }
+        if subject == nil {
+            subject = PublishSubject<Change<T>>()
+            subjects[key] = subject
+        }
+        return subject.asObserver()
+    }
+    
+    func write<T>(_ value: T, key: String) throws where T : Encodable {
+        if isJSONCodable(type: T.self) {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(value)
+            writeData(key: key, data: data)
+        } else {
+            writeValue(key: key, value: value)
+        }
+        (subjects[key] as? PublishSubject<Change<T>>)?.onNext(.update(value))
+    }
+    
+    func remove(key: String) {
+        removeData(key: key)
+        let subject = subjects[key]
+        (subject as? PublishSubject<Change<Any>>)?.onNext(.remove)
+    }
+    
+    func removeAll() {
+        userDefaults.removeObject(forKey: name)
+        userDefaults.synchronize()
+    }
+    
+    func read<T>(key: String) -> T? where T : Decodable {
+        if isJSONCodable(type: T.self) {
+            let decoder  = JSONDecoder()
+            return readData(key: key).flatMap({ try? decoder.decode(T.self, from: $0) })
+        } else {
+            return readValue(key: key) as? T
+        }
+    }
+    
+    // MARK: - Private
+    
+    private func isJSONCodable<T>(type: T.Type) -> Bool {
+        return type != Bool.self &&
+            type != String.self &&
+            type != Int.self &&
+            type != UInt.self &&
+            type != Float.self &&
+            type != Double.self
+    }
+    
+    private func readDictionary() -> [String: Any] {
+        return (userDefaults.value(forKey: name) as? [String: Any]) ?? [:]
     }
     
     private func readData(key: String) -> Data? {
+        return readDictionary()[key] as? Data
+    }
+    
+    private func readValue(key: String) -> Any? {
         return readDictionary()[key]
     }
     
@@ -33,45 +102,18 @@ class KeyValueStorage: ReadWriteRepository, ObservableRepository {
         userDefaults.synchronize()
     }
     
+    private func writeValue(key: String, value: Any) {
+        var dictionary = readDictionary()
+        dictionary[key] = value
+        userDefaults.set(dictionary, forKey: name)
+        userDefaults.synchronize()
+    }
+    
     private func removeData(key: String) {
         var dictionary = readDictionary()
         dictionary.removeValue(forKey: key)
         userDefaults.set(dictionary, forKey: name)
         userDefaults.synchronize()
-    }
-    
-    // MARK: - ObservableRepository
-    
-    func observe<T>(key: String) -> Observable<RepositoryChange<T>> {
-        var subject: PublishSubject<RepositoryChange<T>>! = subjects[key] as! PublishSubject<RepositoryChange<T>>
-        if subject == nil {
-            subject = PublishSubject()
-        }
-        return subject.asObserver()
-    }
-    
-    // MARK: - ReadWriteRepository
-    
-    func write<T>(_ value: T, key: String) throws where T : Decodable, T : Encodable {
-        let encoder = JSONEncoder()
-        let data = try encoder.encode(value)
-        writeData(key: key, data: data)
-        subjects[key]?.onNext(.upate(value))
-    }
-    
-    func remove(key: String) {
-        removeData(key: key)
-        subjects[key]?.onNext(.remove)
-    }
-    
-    func removeAll() {
-        userDefaults.removeObject(forKey: name)
-        userDefaults.synchronize()
-    }
-    
-    func read<T>(key: String) -> T? where T : Decodable, T : Encodable {
-        let decoder  = JSONDecoder()
-        return readData(key: key).flatMap({ try? decoder.decode(T.self, from: $0) })
     }
     
 }
--- Tests/Utils/KeyValueStorageTests.swift
+++ Tests/Utils/KeyValueStorageTests.swift
@@ -4,20 +4,74 @@ import RxSwift
 
 @testable import XcodePM
 
+struct TestModel: Codable, Equatable {
+    let name: String
+    static func ==(lhs: TestModel, rhs: TestModel) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
+
 final class KeyValueStorageTests: XCTestCase {
     
     var subject: KeyValueStorage!
     
     override func setUp() {
         super.setUp()
-//        subject = KeyValueStorage(name: "test")
+        subject = KeyValueStorage(name: "test")
     }
     
     override func tearDown() {
+        super.tearDown()
+        subject.removeAll()
     }
     
     func test_write_writesTheCorrectValue() throws {
-
+        let model = TestModel(name: "waka")
+        try subject.write(model, key: "shakira")
+        let got: TestModel? = subject.read(key: "shakira")
+        XCTAssertEqual(got, model)
     }
     
+    func test_write_writesNonCodableValues() throws {
+        try subject.write("waka", key: "shakira")
+        let got: String? = subject.read(key: "shakira")
+        XCTAssertEqual(got, "waka")
+    }
+    
+    func test_remove_removesTheValue() throws {
+        let model = TestModel(name: "waka")
+        try subject.write(model, key: "shakira")
+        subject.remove(key: "shakira")
+        let got: TestModel? = subject.read(key: "shakira")
+        XCTAssertNil(got)
+    }
+    
+    func test_removeAll_removesAllValues() throws {
+        let model = TestModel(name: "waka")
+        try subject.write(model, key: "shakira")
+        try subject.write(model, key: "shakira2")
+        subject.removeAll()
+        let got1: TestModel? = subject.read(key: "shakira")
+        let got2: TestModel? = subject.read(key: "shakira2")
+        XCTAssertNil(got1)
+        XCTAssertNil(got2)
+    }
+    
+    func test_observe_notifiesWhenAValueChanges() throws {
+        var got: String?
+        _ = subject.observe(key: "shakira").subscribe(onNext: { (change: KeyValueStorage.Change<String>) in
+            got = change.value
+        })
+        try subject.write("waka", key: "shakira")
+        XCTAssertEqual(got, "waka")
+    }
+    
+    func test_observe_notifiesWhenAValueIsRemoved() throws {
+        var got: String? = "value"
+        _ = subject.observe(key: "shakira").subscribe(onNext: { (change: KeyValueStorage.Change<String>) in
+            got = change.value
+        })
+        subject.remove(key: "shakira")
+        XCTAssertNil(got)
+    }
 }
